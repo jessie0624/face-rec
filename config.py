ef744988cf9e1b3e2d6784db3939b943f05f28c8
JessieLi <jessieli@JessieLis-MacBook-Pro.local>
2020-06-22 09:54:56 +0800

    initial commit

---
+++ config.py
@@ -0,0 +1,39 @@
+import torch
+
+
+configurations = {
+    1: dict(
+        SEED = 1337, # random seed for reproduce results
+        # DATA_ROOT = '/disk/tangyike/FaceRec/VGGFace2/test',
+        DATA_ROOT = '/disk/jie/CV/data/faces_emore', # the parent root where your train/val/test data are stored
+        VAL_ROOT = '/disk/jie/CV/data/val',
+        MODEL_ROOT = '/disk/jie/CV/face.evoLVe.PyTorch/checkpoint/', # the root to buffer your checkpoints
+        LOG_ROOT = '/disk/jie/CV/face.evoLVe.PyTorch/log', # the root to log your train/val status
+        BACKBONE_RESUME_ROOT = '/disk/jie/CV/face.evoLVe.PyTorch/checkpoint/Backbone_IR_152_Epoch_1_Batch_45489_Time_2020-06-18-17-53_checkpoint.pth', # the root to resume training from a saved checkpoint
+        HEAD_RESUME_ROOT = '/disk/jie/CV/face.evoLVe.PyTorch/checkpoint/Head_ArcFace_Epoch_1_Batch_45489_Time_2020-06-18-17-53_checkpoint.pth', # the root to resume training from a saved checkpoint
+        # BACKBONE_RESUME_ROOT='',
+        # HEAD_RESUME_ROOT='',
+        BACKBONE_NAME = 'IR_152', # support: ['ResNet_50', 'ResNet_101', 'ResNet_152', 'IR_50', 'IR_101', 'IR_152', 'IR_SE_50', 'IR_SE_101', 'IR_SE_152']
+        HEAD_NAME = 'ArcFace', # support:  ['Softmax', 'ArcFace', 'CosFace', 'SphereFace', 'Am_softmax']
+        LOSS_NAME = 'Focal', # support: ['Focal', 'Softmax']
+
+        INPUT_SIZE = [112, 112], # support: [112, 112] and [224, 224]
+        RGB_MEAN = [0.5, 0.5, 0.5], # for normalize inputs to [-1, 1]
+        RGB_STD = [0.5, 0.5, 0.5],
+        EMBEDDING_SIZE = 512, # feature dimensionpip
+        BATCH_SIZE = 128,
+        DROP_LAST = True, # whether drop the last batch to ensure consistent batch_norm statistics
+        LR = 0.00001, # initial LR ## 要改成 0.001
+        NUM_EPOCH = 5, # total epoch number (use the firt 1/25 epochs to warm up)
+        WEIGHT_DECAY = 5e-4, # do not apply to batch_norm parameters
+        MOMENTUM = 0.9,
+        STAGES = [35, 65, 95], # epoch stages to decay learning rate # 30，60，95
+
+        DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu"),
+        MULTI_GPU = False, # flag to use multiple GPUs; if you choose to train with single GPU, you should first run "export CUDA_VISILE_DEVICES=device_id" to specify the GPU card you want to use
+        GPU_ID = [0], # specify your GPU ids
+        PIN_MEMORY = True,
+        NUM_WORKERS = 2,
+        GRAD_ACC_STEPS = 2,#gradient_accumulation_steps
+),
+}
